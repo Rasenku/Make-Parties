559729d5df33b02847619ce3da6bd410bb3fbf4d
Dahveyea <dahveyea.cowan@gmail.com>
2020-06-03 18:12:49 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,76 @@
+const bodyParser = require('body-parser');
+const models = require('./db/models');
+
+// Initialize express
+const express = require('express')
+const app = express()
+// require handlebars
+var exphbs = require('express-handlebars');
+
+// Use "main" as our default layout
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+// Use handlebars to render
+app.set('view engine', 'handlebars');
+// Tell our app to send the "hello world" message to our home page
+// app.get('/', (req, res) => {
+//     res.render('home', { msg: 'Handlebars are Cool!' });
+// })
+
+// OUR MOCK ARRAY OF PROJECTS
+var events = [
+    { title: "I am your first event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
+    { title: "I am your second event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
+    { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
+  ]
+
+  // INDEX
+  app.get('/', (req, res) => {
+    models.Event.findAll({ order: [['createdAt', 'DESC']] }).then(events => {
+      res.render('events-index', { events: events });
+  })
+})
+
+// Choose a port to listen on
+const port = process.env.PORT || 3000;
+
+// Tell the app what port to listen on
+app.listen(port, () => {
+  console.log('App listening on port 3000!')
+})
+
+// NEW
+app.get('/events/new', (req, res) => {
+  res.render('events-new', {});
+})
+
+// INITIALIZE BODY-PARSER AND ADD IT TO APP
+// const bodyParser = require('body-parser');
+
+
+// The following line must appear AFTER const app = express() and before your routes!
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// CREATE
+app.post('/events', (req, res) => {
+  models.Event.create(req.body).then(event => {
+    res.redirect(`/`);
+    // Redirect to events/:id
+    res.redirect(`/events/${event.id}`)
+
+  }).catch((err) => {
+    console.log(err)
+  });
+})
+
+// SHOW
+app.get('/events/:id', (req, res) => {
+  // Search for the event by its id that was passed in via req.params
+  models.Event.findByPk(req.params.id).then((event) => {
+    // If the id is for a valid event, show it
+    res.render('events-show', { event: event })
+  }).catch((err) => {
+    // if they id was for an event not in our db, log an error
+    console.log(err.message);
+  })
+})
